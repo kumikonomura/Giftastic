d125535f16801f149c7075a828c4b378faffa5ac
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-03-27 15:07:26 -0700

    created function to get gifs from api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,37 @@
-// Whenever add button is clicked
-document.querySelector('#add').addEventListener('click', e => {
-    // make sure the page does not refresh when we get our item
-    e.preventDefault()
-    // Get information from input
-    let item = document.querySelector('#search').value
-    // set item to empty string so text input clears
-    // everytime add is clicked the gifs will be pushed into the gifs array
-    document.querySelector('#search').value = ''
-    console.log(item)
-})
-
-// Get item I search for to appear on HTML
+// Function to create button with every new search
+function createButton() {
+    // Click event listener whenver submit is clicked
+    document.querySelector('#submit').addEventListener('click', e => {
+        // to prevent page from refreshing every time we submit
+        e.preventDefault()
+        // Obtain information from input
+        let item = document.querySelector('#search').value
+        console.log(item)
+        // Set item to empty string so text input clears
+        document.querySelector('#search').value = ''
+    })
+}
 
 // Array of Gifs
 let gifs = []
 
-// Need to make a function that will get gifs from the API
-function getGif(search) {
-    document.querySelector('#gifDiv').innerHTML = ''
-    fetch(`https://api.giphy.com/v1/gifs/search?q=${search}&api_key=dc6zaTOxFJmzC&limit=10`)
+// Function to get gifs on HTML
+function getGif(gifs) {
+    document.querySelector('#gifList').innerHTMl = ''
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${gifs}&api_key=dc6zaTOxFJmzC&limit=10`)
     .then(r => r.json())
     .then(gifs => {
-      gifs.data.forEach(gif => {
-        let gifElem = document.createElement('img')
-        gifElem.setAttribute('src', gif.images.fixed_height.url)
-        document.querySelector('#gifList').append(gifElem)
-      })
+        gifs.data.forEach(gif => {
+            let gifElem = document.createElement('img')
+            gifElem.setAttribute('src', gif.images.fixed.height.url)
+            document.querySelector('gifList').append(gifElem)
+        })
     })
-    }
+    .catch(errors => console.log(e))
+}
 
-// Create button everytime I hit search
 
+// everytime submit is clicked, the gifs will be pushed into the gifs array
 
+// Get item I search for to appear on HTML
 
