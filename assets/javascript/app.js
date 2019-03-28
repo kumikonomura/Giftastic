0d20fb87a3e436216e92747fcccbf5ecc749e65f
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-03-27 18:10:30 -0700

    create button is functioning

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,18 @@
 // Function to create button with every new search
 function createButton() {
+    console.log('createButton function running')
+    // Create a variable for new button
+    const newButton = document.createElement('button')
+    // Setting the new button to inner HTML
+    // inner HTML basically just gives a text value
+    newButton.innerHTML = "Click me!"
+    // button has been created and is saved in computer memory under
+    // variable newButton but the computer doesn't know where to put that button yet
+    document.getElementById('button-container').append(newButton)
+
+}
+
+
     // Click event listener whenver submit is clicked
     document.querySelector('#submit').addEventListener('click', e => {
         // to prevent page from refreshing every time we submit
@@ -7,28 +20,31 @@ function createButton() {
         // Obtain information from input
         let item = document.querySelector('#search').value
         console.log(item)
+        // Create a button
+        createButton()
         // Set item to empty string so text input clears
         document.querySelector('#search').value = ''
     })
-}
 
-// Array of Gifs
-let gifs = []
 
-// Function to get gifs on HTML
-function getGif(gifs) {
-    document.querySelector('#gifList').innerHTMl = ''
-    fetch(`https://api.giphy.com/v1/gifs/search?q=${gifs}&api_key=dc6zaTOxFJmzC&limit=10`)
-    .then(r => r.json())
-    .then(gifs => {
-        gifs.data.forEach(gif => {
-            let gifElem = document.createElement('img')
-            gifElem.setAttribute('src', gif.images.fixed.height.url)
-            document.querySelector('gifList').append(gifElem)
-        })
-    })
-    .catch(errors => console.log(e))
-}
+
+// // Array of Gifs
+// let gifs = []
+
+// // Function to get gifs on HTML
+// function getGif(gifs) {
+//     document.querySelector('#gifList').innerHTMl = ''
+//     fetch(`https://api.giphy.com/v1/gifs/search?q=${gifs}&api_key=dc6zaTOxFJmzC&limit=10`)
+//     .then(r => r.json())
+//     .then(gifs => {
+//         gifs.data.forEach(gif => {
+//             let gifElem = document.createElement('img')
+//             gifElem.setAttribute('src', gif.images.fixed.height.url)
+//             document.querySelector('gifList').append(gifElem)
+//         })
+//     })
+//     .catch(errors => console.log(e))
+// }
 
 
 // everytime submit is clicked, the gifs will be pushed into the gifs array
