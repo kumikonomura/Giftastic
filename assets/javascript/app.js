6d9741fab11237db6ef899ceef4d5e046af0502b
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-04-01 17:58:00 -0700

    pause and play gif added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,18 +16,18 @@ function createButton(buttonText) {
 }
 
 
-    // Click event listener whenver submit is clicked
-    document.querySelector('#submit').addEventListener('click', e => {
-        // to prevent page from refreshing every time we submit
-        e.preventDefault()
-        // Obtain information from input
-        let item = document.querySelector('#search').value
-        console.log(item)
-        // Create a button
-        createButton(item)
-        // Set item to empty string so text input clears
-        document.querySelector('#search').value = ''
-    })
+// Click event listener whenver submit is clicked
+document.querySelector('#submit').addEventListener('click', e => {
+    // to prevent page from refreshing every time we submit
+    e.preventDefault()
+    // Obtain information from input
+    let item = document.querySelector('#search').value
+    console.log(item)
+    // Create a button
+    createButton(item)
+    // Set item to empty string so text input clears
+    document.querySelector('#search').value = ''
+})
 
 
 
@@ -38,17 +38,57 @@ let gifs = []
 function getGif(gifs) {
     document.querySelector('#gifList').innerHTMl = ''
     fetch(`https://api.giphy.com/v1/gifs/search?q=${gifs}&api_key=dc6zaTOxFJmzC&limit=10`)
-    .then(r => r.json())
-    .then(gifs => {
-        gifs.data.forEach(gif => {
-            let gifElem = document.createElement('img')
-            console.log(gif)
-            gifElem.setAttribute('src', gif.images.fixed_height.url)
-            document.querySelector('#gifList').append(gifElem)
+        .then(r => r.json())
+        .then(gifs => {
+            gifs.data.forEach(gif => {
+                let gifElem = document.createElement('img')
+                console.log(gif)
+                gifElem.className = 'gif'
+                gifElem.setAttribute('src', gif.images.fixed_height.url)
+                gifElem.setAttribute('data-isanimated', 'true')
+                gifElem.setAttribute('data-animated', gif.images.fixed_height.url)
+                gifElem.setAttribute('data-still', gif.images.fixed_height_still.url)
+                // gifElem.onclick = ({ target }) => {
+                //     console.log('ping')
+                //     // let { animated, still } = target.dataset;
+                //     // console.log(target.dataset)
+                //     console.log(target.dataset)
+                //     if (target.dataset.isAnimated === '1') {
+                //         target.setAttribute('data-isAnimated', '0')
+                //         target.setAttribute('src', target.dataset.animated)
+                //     } else {
+                //         target.setAttribute('data-isAnimated', '1')
+                //         target.setAttribute('src', target.dataset.still)
+                //     }
+                // }
+                document.querySelector('#gifList').append(gifElem)
+            })
         })
-    })
-    .catch(err => console.log(err))
+        .catch(err => console.log(err))
 }
+
+
+document.addEventListener('click', ({ target}) => {
+    if (target.className = 'gif') {
+        console.log(target.dataset)
+        if (target.dataset.isanimated === 'true') {
+            target.setAttribute('data-isanimated', 'false')
+            target.setAttribute('src', target.dataset.animated)
+        } else {
+            target.setAttribute('data-isanimated', 'true')
+            target.setAttribute('src', target.dataset.still)
+        }
+    }
+})
+// Need to work on when new button is clicked, previous button gifs disappear and 
+
+// Click event listener when gif is clicked
+// document.querySelector('gifs').addEventListener('click', ({target}) => {
+//     let {animated, still} = target.dataset
+//     toggle = !toggle
+//     target.setAttribute('src', toggle ? animated : still)
+// })
+// new button gifs appear
 
 
 // everytime submit is clicked, the gifs will be pushed into the gifs array
